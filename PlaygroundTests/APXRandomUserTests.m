8c3e1869c7c9bd51cb27ca201f9f2d496ab53cf0
Raz Elkayam <raz@appoxee.com>
2015-06-24 19:06:36 +0300

    cleaned code

--- PlaygroundTests/APXRandomUserTests.m
+++ PlaygroundTests/APXRandomUserTests.m
@@ -58,16 +58,6 @@
     return keyedValues;
 }
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testinitialization
 {
     APXRandomUser *randomUser = [[APXRandomUser alloc] initWithKeyedValues:[self getDictionary]];
@@ -111,17 +101,5 @@
     
     XCTAssert([formatedAddress isEqualToString:@"2253 richmond road\nnewport\nborders\n12345"]);
 }
-
-//- (void)testExample {
-//    // This is an example of a functional test case.
-//    XCTAssert(YES, @"Pass");
-//}
-//
-//- (void)testPerformanceExample {
-//    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
-//}
 
 @end
