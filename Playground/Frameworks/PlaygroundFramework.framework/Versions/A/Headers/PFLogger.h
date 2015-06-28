46aef0022ed4ff4848dd58b0916d5fe5162228b6
Raz Elkayam <raz@appoxee.com>
2015-06-28 15:21:56 +0300

    Added Framework under VC

---
+++ Playground/Frameworks/PlaygroundFramework.framework/Versions/A/Headers/PFLogger.h
@@ -0,0 +1,18 @@
+//
+//  PFLogger.h
+//  PlaygroundFramework
+//
+//  Created by Raz Elkayam on 6/23/15.
+//  Copyright (c) 2015 Raz Elkayam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface PFLogger : NSObject
+
++ (BOOL)logMessage:(NSString *)string;
+
+
+
+@end
---
+++ Playground/Frameworks/PlaygroundFramework.framework/Versions/A/Headers/PlaygroundFramework.h
@@ -0,0 +1,11 @@
+//
+//  PlaygroundFramework.h
+//  PlaygroundFramework
+//
+//  Created by Raz Elkayam on 6/23/15.
+//  Copyright (c) 2015 Raz Elkayam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <PlaygroundFramework/PFLogger.h>
